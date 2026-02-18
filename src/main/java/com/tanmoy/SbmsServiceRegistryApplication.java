8b4b3789ef95c6244d8fa60037810adcfb90c2fe
Tanmoy <hazratanmoy62@gmail.com>
2026-02-18 23:57:25 +0530

    added code in property file and start class

--- src/main/java/com/tanmoy/SbmsServiceRegistryApplication.java
+++ src/main/java/com/tanmoy/SbmsServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.tanmoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SbmsServiceRegistryApplication {
 
 	public static void main(String[] args) {
