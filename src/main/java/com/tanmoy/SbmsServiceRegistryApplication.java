4ea75dcb30f55dac6d23cc72d5d84a88504af0cc
Tanmoy <hazratanmoy62@gmail.com>
2026-02-18 23:13:35 +0530

    Adding starting of Service registry project

---
+++ src/main/java/com/tanmoy/SbmsServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.tanmoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsServiceRegistryApplication.class, args);
+	}
+
+}
